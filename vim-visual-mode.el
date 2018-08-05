408c1057455b2a15a8dbaf63d7ed00d67f3a6c84
meretciel <meretciel.fr@gmail.com>
2018-08-05 12:54:40 -0400

    first commit

---
+++ vim-visual-mode.el
@@ -0,0 +1,16 @@
+
+(region-beginning)
+(region-end)
+(region-active-p)
+
+
+beginning some text and end
+
+(set-mark-command (point))
+
+
+(set-cursor-color)
+
+(frame-parameter nil 'cursor-color)
+
+global-constant-cursor-color
